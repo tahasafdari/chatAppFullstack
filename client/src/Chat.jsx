673c0b19dec121a446d75e315b24408273c29b87
tahasafdari <tahahasan.safdari@gmail.com>
2023-03-19 13:05:17 +0200

    user can see now the online persons in the contact field

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
-import QuestionAnswerRoundedIcon from "@mui/icons-material/QuestionAnswerRounded";
+import Logo from "./Logo";
 import Avatar from "./Avatar";
+import { UserContext } from "./UserContext";
+
 export default function Chat() {
   const [websocket, setWebsocket] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const { username, id } = useContext(UserContext);
+  const [newMessageText, setNewMessageText] = useState(null);
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:4000");
@@ -25,23 +30,42 @@ export default function Chat() {
       showOnlinePeople(messageData.online);
     }
   }
+
+  const onlinePeopleExclOurUser = { ...onlinePeople };
+  delete onlinePeopleExclOurUser[id];
+
   return (
     <div className="flex h-screen">
-      <div className="bg-white w-1/3 pl-4 pt-4">
-        <div className="text-blue-600 font-bold flex gap-3 mb-4">
-          <QuestionAnswerRoundedIcon className="text-gray-700" />
-          CHAT APP
-        </div>
-        {Object.keys(onlinePeople).map((p) => (
-          <div id="onlinePeople" className="border-b border-gray-100 py-2">
-            <div className="w-4 h-4 bg-red-200"></div>
-            <Avatar />
-            {onlinePeople[p]}
+      <div className="bg-white w-1/3 ">
+        <Logo />
+        {Object.keys(onlinePeopleExclOurUser).map((p) => (
+          <div
+            key={p}
+            onClick={() => setSelectedUserId(p)}
+            id="onlinePeople"
+            className={
+              "border-b border-gray-100 flex items-center gap-2 cursor-pointer " +
+              (selectedUserId === p ? "bg-blue-50" : "")
+            }
+          >
+            {p === selectedUserId && (
+              <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+            )}
+            <div className="flex gap-2 py-2 pl-4 items-center">
+              <Avatar username={onlinePeople[p]} userId={p} />
+              <span className="text-gray-700">{onlinePeople[p]}</span>
+            </div>
           </div>
         ))}
       </div>
       <div className="flex flex-col bg-blue-50 w-2/3 p-2">
-        <div className="flex-grow">messages with selected person!</div>
+        <div className="flex-grow">
+          {!selectedUserId && (
+            <div className="flex h-full flex-grow items-center justify-center">
+              <div className="text-gray-300">&larr; Select a person </div>
+            </div>
+          )}
+        </div>
         <div className="flex gap-2 rounded-sm">
           <input
             type={"text"}
